144f5de4b85e118af2829058123e8df15538b66a
BrettonSteiner <imbrettonsteiner@gmail.com>
2019-11-13 12:24:43 -0600

    Team09: __dirname works! Now applying to views folder path.

--- week09/TeamActivity/server.js
+++ week09/TeamActivity/server.js
@@ -4,7 +4,7 @@ var path = require('path');
 var app = express();
 
 app.use(express.static(__dirname+'/public'));
-app.set('views', 'views');
+app.set('views', __dirname+'/views');
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res) {
